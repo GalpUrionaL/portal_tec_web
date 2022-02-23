677c0041316816c8f8d3e8719e1bb4d4b36f9947
PaoUrionaL <bunny.301275@gmail.com>
2022-02-23 10:28:36 -0400

    update

---
+++ Practica_FizzBuzz/js/fizzBuzz.js
@@ -0,0 +1,16 @@
+for (let i = 1; i <= 100; i++) {
+    if (((i % 3) == 0) && ((i % 5) == 0)) {
+        document.write(i + ".-" + " " + "FizzBuzz");
+        document.write("<br>");
+    } else if ((i % 3) == 0) {
+        document.write(i + ".-" + " " + "Fizz");
+        document.write("<br>");
+    } else if ((i % 5) == 0) {
+        document.write(i + ".-" + " " + "Buzz");
+        document.write("<br>");
+    } else {
+        document.write(i + ".-");
+        document.write("<br>");
+    }
+
+}
