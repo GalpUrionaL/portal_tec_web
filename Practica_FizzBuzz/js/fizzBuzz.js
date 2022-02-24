2feddf0008bdcc5eb36ae0968f9f7ee8489cc603
PaoUrionaL <bunny.301275@gmail.com>
2022-02-24 15:56:39 -0400

    update

--- Practica_FizzBuzz/js/fizzBuzz.js
+++ Practica_FizzBuzz/js/fizzBuzz.js
@@ -1,16 +1,21 @@
-for (let i = 1; i <= 100; i++) {
-    if (((i % 3) == 0) && ((i % 5) == 0)) {
-        document.write(i + ".-" + " " + "FizzBuzz");
-        document.write("<br>");
-    } else if ((i % 3) == 0) {
-        document.write(i + ".-" + " " + "Fizz");
-        document.write("<br>");
-    } else if ((i % 5) == 0) {
-        document.write(i + ".-" + " " + "Buzz");
-        document.write("<br>");
-    } else {
-        document.write(i + ".-");
-        document.write("<br>");
-    }
+function FizzBuzz(num) {
 
+    for (let i = 1; i <= num; i++) {
+        if (((i % 3) == 0) && ((i % 5) == 0)) {
+            document.write(i + ".-" + " " + "FizzBuzz");
+            document.write("<br>");
+        } else if ((i % 3) == 0) {
+            document.write(i + ".-" + " " + "Fizz");
+            document.write("<br>");
+        } else if ((i % 5) == 0) {
+            document.write(i + ".-" + " " + "Buzz");
+            document.write("<br>");
+        } else {
+            document.write(i + ".-");
+            document.write("<br>");
+        }
+
+    }
 }
+
+FizzBuzz(15);
